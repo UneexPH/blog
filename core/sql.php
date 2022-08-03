943170d0fd70c656ac13516af40cec9df6ccc06c
pedro <pedro_arrieiro@hotmail.com>
2022-08-03 10:49:25 -0300

    a

---
+++ core/sql.php
@@ -0,0 +1,55 @@
+<?php
+
+function insert (string $entidade, array $dados) : String 
+{
+$instrucao = "INSERT INTO {$entidade}";
+
+$campos = implode(', ', array_keys($dados));
+$valores = implode(', ', array_values($dados));
+
+$instrucao.= "({$campos})";
+$instrucao .= "VALUES ({$valores})";
+
+return $instrucao;
+}
+
+/* */
+
+function delete (string $entidade, array $criterio = []) : String
+{
+    $instrucao = "DELETE {$entidade}";
+
+    if(!empty($criterio)){
+        $instrucao .=' WHERE ';
+
+        foreach($criterio as $expressao){
+            $instrucao .=' ' . implode(' ', $expressao);
+        }
+    }
+
+    return $instrucao;
+}
+
+
+
+function select (string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+{
+    $instrucao = "SELECT " . IMPLODE (', ' ,$campos);
+    $instrucao .=" FROM {$entidade}";
+
+    if(!empty($criterio)){
+        $instrucao .= ' WHERE ';
+
+        foreach($criterio as $expressao){
+            $instrucao .=' ' . implode(' ', $expressao);
+        }
+    }
+
+    if(!empty($ordem)){
+        $instrucao .= "ORDER BY $ordem ";
+    }
+
+    return $instrucao;
+}
+
+?>
