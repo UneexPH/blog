a12d7fffaf4229eb08c24831c9e5e9b4490e132d
pedro <pedro_arrieiro@hotmail.com>
2022-08-03 12:06:19 -0300

    a

--- core/sql.php
+++ core/sql.php
@@ -8,31 +8,12 @@ $campos = implode(', ', array_keys($dados));
 $valores = implode(', ', array_values($dados));
 
 $instrucao.= "({$campos})";
-$instrucao .= "VALUES ({$valores})";
+$instrucao .= " VALUES ({$valores})";
 
 return $instrucao;
 }
 
-function update (string $entidade, array $dados, array $criterio = []) : string
-{
-    $instrucao = "UPDATE{$entidade}";
-
-    foreach ($dados as $campo => $dados){
-        $set[] = "{$campo}" = {$dados}";
-    }
-
-    $instrucao .= ' SET ' . implode(',', $set) ;
-
-    if (!empty($criterio)){
-        $intrucao .= ' WHERE';
-        
-        foreach($criterio as $expressao) {
-            $instrucao .= ' '. implode (' ',$expressao);
-        }
-    }
-
-    return $instrucao;
-}
+/* */
 
 function delete (string $entidade, array $criterio = []) : String
 {
@@ -49,24 +30,42 @@ function delete (string $entidade, array $criterio = []) : String
     return $instrucao;
 }
 
-function select (string $entidade, array $campos, array $criterio = [], 
-string $ordem = null) : string
+function update (string $entidade, array $dados, array $criterio = []) : string
 {
-    $instrucao = "SELECT " . IMPLODE (', ' ,$campos);
-    $instrucao .=" FROM {$entidade}";
+    $instrucao = "UPDATE {$entidade}";
+
+    foreach ($dados as $campo => $dado)
+    {
+        $set[] = "{$campo} = {$dado}";
+    }
+    $instrucao .= ' SET ' . implode(',', $set) ;
+
+    if (!empty($criterio)){
+        $instrucao .= ' WHERE';
+        
+        foreach($criterio as $expressao) {
+            $instrucao .= ' '. implode (' ',$expressao);
+        }
+    }
+    return $instrucao;
+}
+
+function  select (string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+{
+    $instrucao = " SELECT " . implode(',',$campos);
+    $instrucao .= " FROM {$entidade}";
 
     if(!empty($criterio)){
         $instrucao .= ' WHERE ';
 
-        foreach($criterio as $expressao){
-            $instrucao .=' ' . implode(' ', $expressao);
+        foreach ($criterio as $expressao){
+            $instrucao .= ' '.implode (' ', $expressao);
         }
     }
-
     if(!empty($ordem)){
         $instrucao .= " ORDER BY $ordem ";
     }
-
     return $instrucao;
 }
+
 ?>
