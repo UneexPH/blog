5e7dd390b7e67297cc34b21a19a317a16b7ee0ce
pedro <pedro_arrieiro@hotmail.com>
2022-08-31 11:53:31 -0300

    a

--- login_formulario.php
+++ login_formulario.php
@@ -14,7 +14,7 @@
             </div>
             <div class="row" style="min-height: 500px;">
                 <div class="col-md-12">
-                    <?php include 'include/menu.php'; ?>
+                    <?php include 'includes/menu.php'; ?>
                 </div>
                 <div class="col-md-10" style="padding-top: 50px;">
                     <div class="card-header">Login</div>
